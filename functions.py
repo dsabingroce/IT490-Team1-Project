c6f524ab14d357ff4a124f1cc12263a7cbb71cb6
dsabingroce <55164827+dsabingroce@users.noreply.github.com>
2019-10-22 01:51:51 -0400

    functions.py

--- functions.py
+++ functions.py
@@ -17,7 +17,7 @@ connection = pyodbc.connect(connect_string)
 #18: Cursor runs queries and fetches rows
 cursor = connection.cursor()
 
-#21-32: Auth function used to see if a user's passwords match up
+#21-33: Auth function used to see if a user's passwords match up
 def auth(uName, pWord):
 	query="SELECT password FROM accounts WHERE username='"+uName+"'"
 	with cursor.execute(query):
@@ -31,18 +31,22 @@ def auth(uName, pWord):
 	#32-33: If a username doesn't exist in the database, you get a type error if you try to put row[0] in a variable.	
 	except TypeError:
 		return "Username does not exist!"
-
-def addUser(uName, pWord, first, last):
+#35-52: addUser function adds a user's credentials into the database
+def addUser(username, password, fName, lName):
+	#37-40: Gets a count of the rows and adds one in order to create a primary key for the table
 	query1="SELECT COUNT(*) FROM accounts"
 	with cursor.execute(query1):
 		row=cursor.fetchone()
 	count=row[0]+1
 	
-	#Current problem: Query 2 isn't a query?
-	query2="INSERT INTO accounts (userID,username,password,fName,lName) VALUES ("+str(count)+",\'"+uName+"\',\'"+pWord+"\',\'"+first+"\',\'"+last+"\');"
-	print query2
-	with cursor.execute(query2):
-		row2=cursor.fetchone()
-	print row2[0]
-
-addUser("test4", "efgh", "Tim", "Scott")
+	#43-52: Inserts the user's row into the database
+	query2="INSERT INTO accounts VALUES ("+str(count)+",'"+username+"','"+password+"','"+fName+"','"+lName+"','',0,0,0,0,0,0,0,'',0)"
+	try:
+		cursor.execute(query2)
+		connection.commit()
+		print "Account created!"
+		return "Account created!"
+	#50-52: If a username is already taken, it gives an intergrity error
+	except pyodbc.IntegrityError:
+		print "Username is already in use"		
+		return "Username is already in use"
