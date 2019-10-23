816e5ed7c2aa670b06064db54cca8c7c19ad1d50
dsabingroce <55164827+dsabingroce@users.noreply.github.com>
2019-10-22 20:42:28 -0400

    Functions for the database

--- Database/functions.py
+++ Database/functions.py
@@ -25,28 +25,42 @@ def auth(uName, pWord):
 	try: 
 		password=row[0]
 		if pWord==password:
-			return "Passwords match!"
+			return "true"
 		else:
-			return "Passwords do not match!"
+			return "false"
 	#32-33: If a username doesn't exist in the database, you get a type error if you try to put row[0] in a variable.	
 	except TypeError:
-		return "Username does not exist!"
-#35-52: addUser function adds a user's credentials into the database
+		return "false"
+
+#36-51: addUser function adds a user's credentials into the database
 def addUser(username, password, fName, lName):
-	#37-40: Gets a count of the rows and adds one in order to create a primary key for the table
+	#38-41: Gets a count of the rows and adds one in order to create a primary key for the table
 	query1="SELECT COUNT(*) FROM accounts"
 	with cursor.execute(query1):
 		row=cursor.fetchone()
 	count=row[0]+1
 	
-	#43-52: Inserts the user's row into the database
+	#44-51: Inserts the user's row into the database
 	query2="INSERT INTO accounts VALUES ("+str(count)+",'"+username+"','"+password+"','"+fName+"','"+lName+"','',0,0,0,0,0,0,0,'',0)"
 	try:
 		cursor.execute(query2)
 		connection.commit()
-		print "Account created!"
-		return "Account created!"
-	#50-52: If a username is already taken, it gives an intergrity error
-	except pyodbc.IntegrityError:
-		print "Username is already in use"		
-		return "Username is already in use"
+		return "true"
+	#50-51: If a username is already taken, it gives an intergrity error
+	except pyodbc.IntegrityError:		
+		return "false"
+
+#54-66: addScores adds a user's scores to their account
+def addScores(user, timeTrav, songLis, pop, rock, country, hiphop, edm,):
+	#56-57: Puts the new values into a dictionary to iterate through with the Select statement	
+	new=[timeTrav,songLis,pop,rock,country,hiphop,edm]	
+	query1="SELECT timeTraveled, songsListenedTo, pop, rock, country, hiphop, edm FROM accounts WHERE username='"+user+"'"
+	with cursor.execute(query1):
+		row=cursor.fetchone()
+		#61-62: Adds the old scores with the new scores to put them back into the table		
+		for i in range(len(row)):
+			new[i]=new[i]+row[i]
+	#64-66: Updates the row with the new scores and commits them
+	query2="UPDATE accounts SET timeTraveled="+str(new[0])+", songsListenedTo="+str(new[1])+", pop="+str(new[2])+", rock="+str(new[3])+", country="+str(new[4])+", hiphop="+str(new[5])+", edm="+str(new[6])+" WHERE username='"+user+"'"
+	cursor.execute(query2)
+	connection.commit()
