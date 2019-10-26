972e55c65365eabee4ff659c52178caac27195a4
castaldoj <49074278+castaldoj@users.noreply.github.com>
2019-10-26 19:58:11 -0400

    Update trackRead.py

--- DMZ/trackRead.py
+++ DMZ/trackRead.py
@@ -4,7 +4,10 @@ import json
 from pprint import pprint
 import subprocess
 
-batcmd="cat trackID.json | grep 'spotify:track'"
+#batcmd="cat trackID.json | grep 'spotify:track'"
+
+batcmd="cat trackID.json | grep 'spotify:track' | sed 's/    \"uri\" : \"//g' | sed 's/\"//g'"
+
 results = subprocess.check_output(batcmd, shell=True)
 
 with open("tracks.py", "w") as f:
