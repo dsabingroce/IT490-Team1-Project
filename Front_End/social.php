832e325f4320d46bcbe312b7c2e8e7ac787d6f67
NEWguy70 <36432993+NEWguy70@users.noreply.github.com>
2019-10-26 16:26:55 -0400

    Create social.php

---
+++ Front_End/social.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+<main>
+  <div class="Square">
+
+
+<body>
+    
+<font color = "#0000cd"><h1>Social</h1></font>
+    <form action ="main_menu.html" method="post">
+        
+	
+      <!--  <h3 align="left">Add friend: </h3> -->
+	<table>
+	    <tr>
+		<h3 align="left">Current friends: </h3>
+	     </tr>
+		<br>
+		<br>
+		<br>
+            <tr>
+                <td><h3 align="left">Add friend: </h3></td>
+		<td><input type="text" name="questionName"></td>
+		<td><input align="right" type="button" 			onclick="location.href='social.php';"value='enter'></td>
+	 </tr>
+	
+	</table>
+
+
+	<br>
+	<br>
+
+        <input type="submit" name="submit" value="Main Menu"></td> 
+            </tr> 
+	
+        </table>
+    </form>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+</main>
+</body>
+</html>
