a504cd12a7572270183033fbcb58083ecb4bf858
NEWguy70 <36432993+NEWguy70@users.noreply.github.com>
2019-10-26 16:21:57 -0400

    Create score_board.php

---
+++ Front_End/score_board.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+<main>
+  <div class="Square">
+
+
+<body>
+    
+<font color = "#0000cd"><h1>Score Board</h1></font>
+    <form action ="main_menu.html" method="post">
+        
+	<h3 align="left">Username: </h3>
+        <h3 align="left">Total Time Traveled: </h3>
+        <h3 align="left">Total number of songs listened to:</h3>
+	<h3 align="left">Pop:</h3>
+	<h3 align="left">Rock:</h3>
+	<h3 align="left">Country:</h3>
+	<h3 align="left">Hip-Hop:</h3>
+	<h3 align="left">EDM:</h3>
+	<br>
+	<br>
+
+        <input type="submit" name="submit" value="Main Menu"></td> 
+            </tr> 
+	
+        </table>
+    </form>
+</body>
+
+
+
+
+</main>
+</body>
+</html>
