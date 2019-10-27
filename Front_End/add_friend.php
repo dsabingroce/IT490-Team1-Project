9d120676e24d681c39265b95e951133ffed7b3e5
NEWguy70 <36432993+NEWguy70@users.noreply.github.com>
2019-10-26 20:50:58 -0400

    Create add_friend.php

---
+++ Front_End/add_friend.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Social</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+<main>
+  <div class="Square">
+
+
+<body>
+
+
+<?php
+
+
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+class RpcClient
+{
+    private $connection;
+    private $channel;
+    private $callback_queue;
+    private $response;
+    private $corr_id;
+
+    public function __construct()
+    {
+        $this->connection = new AMQPStreamConnection(
+            '192.168.1.5',
+            5672,
+            'FE',
+            'FE_1234'
+        );
+        $this->channel = $this->connection->channel();
+        list($this->callback_queue, ,) = $this->channel->queue_declare(
+            "DB_addFriend",
+            false,
+            true,
+            false,
+            false
+        );
+        $this->channel->basic_consume(
+            $this->callback_queue,
+            '',
+            false,
+            true,
+            false,
+            false,
+            array(
+                $this,
+                'onResponse'
+            )
+        );
+    }
+
+    public function onResponse($rep)
+    {
+        if ($rep->get('correlation_id') == $this->corr_id) {
+            $this->response = $rep->body;
+        }
+    }
+
+    public function call($n)
+    {
+        $this->response = null;
+        $this->corr_id = uniqid();
+
+        $msg = new AMQPMessage(
+            $n,
+            array(
+                'correlation_id' => $this->corr_id,
+                'reply_to' => $this->callback_queue
+            )
+        );
+        $this->channel->basic_publish($msg, '', 'DB_addFriend');
+        while (!$this->response) {
+            $this->channel->wait();
+        }
+        return ($this->response);
+    }
+}
+
+$add_friend = $_POST['addFriend'];
+
+$rpc = new RpcClient();
+$response = $rpc->call("$add_friend");
+
+
+header('Location: social.php');
+		
+
+
+?>
+
+</body>
+
+
+
+</main>
+</body>
+</html>
