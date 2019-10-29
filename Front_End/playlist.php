f8b85f64cdd0c5ddd6fc83087245aee2a1ab06c6
NEWguy70 <36432993+NEWguy70@users.noreply.github.com>
2019-10-29 16:22:13 -0400

    Update playlist.php

--- Front_End/playlist.php
+++ Front_End/playlist.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Playlist</title>
+    <title>Select genre</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
@@ -12,6 +12,12 @@
 
         <?php
 
+		if (isset($_GET['time'])) {
+
+			$time = $_GET['time'];
+	
+		} else {
+
        # $email = $_POST['email'];
         $Origin_streetName = $_POST['Origin_streetName'];
         $Origin_cityName = $_POST['Origin_cityName'];
@@ -19,7 +25,11 @@
 	$Origin_zipcode = $_POST['Origin_Zipcode'];
 	
 	$Origin_Full = $Origin_streetName + $Origin_cityName + $Origin_stateName + $Origin_zipcode;
+
+	$save_destination_box = $_POST['save_destination_box'];
 	
+		
+
 	#echo $Origin_Full;
 
 #	echo ("{'locations':['".$Origin_streetName." ".$Origin_cityName." ".$Origin_stateName." ".$Origin_zipcode."']}");
@@ -48,6 +58,8 @@
 #	echo $Des_zipcode;
 
 $location = ('{"locations":["'.$Origin_streetName.' '.$Origin_cityName.' '.$Origin_stateName.' '.$Origin_zipcode.'","'.$Des_streetName.' '.$Des_cityName.' '.$Des_stateName.' '.$Des_zipcode.'"]}');
+
+}
 
 require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
@@ -98,6 +110,19 @@ class RpcClient
         }
     }
 
+	public function send_time($message) {
+		$this->channel->exchange_declare('DMZ_saved', 'direct', false, true, false);	
+		$msg = new AMQPMessage($message);
+		$this->channel->basic_publish($msg, 'DMZ_saved');
+
+	}
+
+    public function one_way($message) {
+		$this->channel->exchange_declare('DB_saveRoute', 'direct', false, true, false);	
+		$msg = new AMQPMessage($message);
+		$this->channel->basic_publish($msg, 'DB_saveRoute');
+    }
+
     public function call($n)
     {
         $this->response = null;
@@ -122,11 +147,27 @@ class RpcClient
 
 ##############################################################
 $rpc = new RpcClient();
-$response = $rpc->call("$location");
+
+if (! isset($_GET['time'])) {
+	$response = $rpc->call("$location");
+	if ($save_destination_box == "true") {
+		#echo "sending route to DB";
+		$rpc->one_way("$response");	
+	}
+} else {
+	$rpc->send_time("$time");
+	$response = $time;
+}
+#echo "the checkbox is: $save_destination_box";
+
+
+##############################################################
 
 #echo $response;
 $total_minutes = gmdate("H:i:s", $response);
 echo "You will arrive in " .$total_minutes. " minutes.";
+
+
 
 
 
@@ -139,12 +180,12 @@ echo "You will arrive in " .$total_minutes. " minutes.";
 <input type="text" name="name_of_playlist" id="name_of_playlist" placeholder="Name the playlist">
 
 
-	<table>
+
 <!------------------------------------------------------------------>
             <h1>Select Genre</h1>
             
 
-              <!--  <table> -->
+              <!--  <table> 
                     <tr>
                         <td>Country:</td>
                         <td><input type="checkbox" value="true" id="country_music" name="country"></td>
@@ -170,13 +211,45 @@ echo "You will arrive in " .$total_minutes. " minutes.";
                     </tr>
 
 
-                </table>
+                </table> 
+
+-->
+
+
+
+	
+	<select name= "genre_selection" id="genre_selection">  
+ 		 <option value="Select">Select a genre</option>}
+		 <option value="Country">Country</option>
+		 <option value="hip-hop">hip-hop</option>
+		 <option value="pop">pop</option>
+		 <option value="edm">edm</option>
+		 <option value="rock">rock</option>
+	</select>
+
+	<br>
+	<br>
+	<td>Save Playlist</td>
+     <input type="checkbox" id="saved_playlist_box" name="saved_playlist_box" value="true"> 
+	
+	<br>
+	<br>
+	<br>
+	<br>
+
                 <input type="submit" name="submit" value="submit">
-                <input type="button" onclick="location.href='new_route.php';"value='back' />
+
+                <input type="button" onclick="location.href='main_menu.php';"value='main menu' />
 
             </form>
-        </center>
-    </div>
+<br>
+<br>
+	</center>
+	</div>
+	
+       
+   
 </main>
 </body>
 </html>
+	
