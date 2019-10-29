148780f26eaa04bad5b693a8604a3eba17c66c40
Christopher Mara <44581616+Chris-Mara@users.noreply.github.com>
2019-10-29 16:12:02 -0400

    Update central_logs.php

--- Logs/central_logs.php
+++ Logs/central_logs.php
@@ -4,15 +4,19 @@ require_once __DIR__ . '/vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+//Determine central logging file
 $log_file = "./central_log";
 
+//Establish connections
 $connection = new AMQPStreamConnection('192.168.1.5', 5672, 'RMQ', 'RMQ_1234');
 $channel = $connection->channel();
 
+//Wait for incoming messages
 $channel->queue_declare('DB_logs', false, true, false, false);
 
 echo " [*] Central Logging Enabled. Waiting for messages. To exit press CTRL+C\n";
 
+//Callback function to write incoming messages to the central logging file
 $callback = function ($msg) {
 	global $log_file;
   	echo ' [x] Received ', $msg->body, "\n";
