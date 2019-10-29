f8c6bede11c3996d2f62b45b5f117a5591ae79a8
NEWguy70 <36432993+NEWguy70@users.noreply.github.com>
2019-10-29 16:13:10 -0400

    Update Your_playlist.php

--- Front_End/Your_playlist.php
+++ Front_End/Your_playlist.php
@@ -1,9 +1,8 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Genre</title>
+    <title>Your Playlist</title>
     <link rel="stylesheet" href="login.css">
 </head>
 <body>
@@ -15,9 +14,10 @@
 
 
 <?php
-
+	$saved_playlist_box = $_POST['saved_playlist_box'];
 	$name_of_playlist = $_POST['name_of_playlist'];
-
+	
+/*
 	$genre_country = $_POST['country'];
 	$genre_hip_hop = $_POST['hip-hop'];
 	$genre_pop = $_POST['pop'];
@@ -42,10 +42,18 @@ elseif($genre_rock){
 else{
 	$music_type= "hip-hop";
 }
+*/
 
+ 
+ $music_type=$_POST ['genre_selection']; 
 
 $genre_check = ('curl -o seed.json -X "GET" "https://api.spotify.com/v1/recommendations?limit=3&market=US&seed_genres='.$music_type.'" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQAJOm4rnh-AQA6FSYVQDOp4yz2R0vx1vk_buPemDsiYkD-eHh25luMIaVqXNuVm0jb2x_aRkU03p5DPILpoxgXwuasZbf7mdTtRByyKtQ5OYdMotbskXvN30dndCp3TJjjRU5-eisdbHXfDpFoZpQGYwLMOLizRIHWAaqYPzqBDPxU"');
 
+
+
+
+
+############################################################################
 
 
 require_once __DIR__ . '/vendor/autoload.php';
@@ -97,6 +105,12 @@ class RpcClient
         }
     }
 
+	public function one_way($message) {
+		$this->channel->exchange_declare('DB_savePlaylist', 'direct', false, true, false);	
+		$msg = new AMQPMessage($message);
+		$this->channel->basic_publish($msg, 'DB_savePlaylist');
+    }
+
     public function call($n)
     {
         $this->response = null;
@@ -120,21 +134,35 @@ class RpcClient
     }
 }
 
-
-
-
-
 $rpc = new RpcClient();
 #$response = $rpc->call("$genre_check");
 $response = $rpc->call("$music_type,$name_of_playlist");
+
+if ($saved_playlist_box == "true") {
+	#echo "sending playlist to DB";
+	$rpc->one_way("$response");	
+}
 
 echo "Please visit the url:   ";
 echo $response;
 
 
+ 
+
+
 
 ?>
 
+<br>
+<br>
+<br>
+     
+<br>
+<br>
+<br>
+
+<input type="button" onclick="location.href='main_menu.php';"value='main menu' />
+	
 </div>
 </main>
 </body>
