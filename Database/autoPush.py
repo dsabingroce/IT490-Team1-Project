a3c6d4714eba9c4cea1aea924dfd98c87c1a662c
dsabingroce <dsabingroce@yahoo.com>
2019-11-12 18:33:33 -0500

    Auto-Updated Script

--- Database/autoPush.py
+++ Database/autoPush.py
@@ -2,5 +2,6 @@ import os
 
 os.system("git add --all")
 os.system("git commit -m 'Auto-Updated Script'")
+os.system("git push origin development")
 os.system("git pull origin staging")
 os.system("git push origin development:staging")
