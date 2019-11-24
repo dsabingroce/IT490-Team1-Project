c7093b8992dd129e3c071d05b83f334d4fa08ae7
dsabingroce <55164827+dsabingroce@users.noreply.github.com>
2019-11-24 14:59:18 -0500

    Delete autoPushStg.py

--- Database/autoPushStg.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("git add --all")
-os.system("git commit -m 'Auto-Updated Script'")
-os.system("git push origin staging")
-os.system("git pull origin master")
-os.system("git push origin staging:master")
