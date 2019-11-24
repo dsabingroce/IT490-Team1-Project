c95675d0cb9d06308022fdb887c99f9b0cb1af22
dsabingroce <55164827+dsabingroce@users.noreply.github.com>
2019-11-24 14:59:07 -0500

    Delete autoPushDev.py

--- Database/autoPushDev.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("git add --all")
-os.system("git commit -m 'Auto-Updated Script'")
-os.system("git push origin development")
-os.system("git pull origin staging")
-os.system("git push origin development:staging")
